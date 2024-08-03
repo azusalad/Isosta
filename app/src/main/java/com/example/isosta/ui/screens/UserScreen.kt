8b4c69ee7839f5bcbe4b88c962a62eaf38a03361
azusalad <68360714+azusalad@users.noreply.github.com>
2024-08-03 13:50:22 -0500

    Updated UserBio(). * Added StatText() for showing user stats like number of posts, followers, and following.
    * Modified the column settings in UserBio() and modified the text stuff.

--- app/src/main/java/com/example/isosta/ui/screens/UserScreen.kt
+++ app/src/main/java/com/example/isosta/ui/screens/UserScreen.kt
@@ -1,14 +1,18 @@
 package com.example.isosta.ui.screens
 
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
@@ -20,6 +24,7 @@ import coil.request.ImageRequest
 import com.example.isosta.R
 import com.example.isosta.model.IsostaUser
 import androidx.compose.material3.Text
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.isosta.model.IsostaComment
@@ -68,7 +73,11 @@ fun UserBio(
     followingCount: Int,
     modifier: Modifier = Modifier
 ) {
-    Column {
+    Column(
+        verticalArrangement = Arrangement.SpaceBetween,
+        horizontalAlignment = Alignment.CenterHorizontally,
+        modifier = modifier.fillMaxWidth()
+    ) {
         // Profile picture
         AsyncImage(
             model = ImageRequest.Builder(context = LocalContext.current)
@@ -81,21 +90,50 @@ fun UserBio(
             contentDescription = "Profile picture",
             contentScale = ContentScale.Fit,
             modifier = Modifier
-                .clip(CircleShape),
+                .clip(CircleShape).size(200.dp),
         )
         // User name
         Text(
-            text = "Kita Ikuyo"
+            text = user.profileName,
+            style = MaterialTheme.typography.displayMedium,
         )
         // User handle
         Text(
-            text = "@kita"
+            text = user.profileHandle,
+            style = MaterialTheme.typography.titleLarge,
         )
-        Row {
-            Text("posts")
-            Text("followers")
-            Text("following")
+        Spacer(
+            modifier = modifier.height(10.dp)
+        )
+        Row(
+            horizontalArrangement = Arrangement.SpaceEvenly,
+            modifier = Modifier.fillMaxWidth().padding(8.dp)
+        ) {
+            StatText(topText = postCount.toString(), bottomText = "posts")
+            StatText(topText = followerCount.toString(), bottomText = "followers")
+            StatText(topText = followingCount.toString(), bottomText = "following")
         }
+    }
+}
+
+@Composable
+fun StatText(
+    topText: String,
+    bottomText: String,
+    modifier: Modifier = Modifier
+) {
+    Column(
+        horizontalAlignment = Alignment.CenterHorizontally,
+        modifier = modifier
+    ) {
+        Text(
+            text = topText,
+            style = MaterialTheme.typography.displaySmall,
+        )
+        Text(
+            text = bottomText,
+            style = MaterialTheme.typography.titleLarge,
+        )
     }
 }
 
